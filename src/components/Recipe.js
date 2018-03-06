87cdff842b559b23b1ecf8468623d058985efc82
YulijaU <cenite-moment@mail.ru>
2018-03-06 08:50:44 +0700

    Displays ingredients in a recipe & styles lists differently

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,5 +1,13 @@
+import {Ingredient} from './Ingredient';
+
 export const Recipe = ({recipe}) => {
     return (
-        <li>{recipe.recipeName ? recipe.recipeName : 'Untitled Recipe'}</li>
+        <li className="recipe-block">
+            <div>{recipe.recipeName ? recipe.recipeName : 'Untitled Recipe'}</div>
+            <ul>{recipe.ingredients ? recipe.ingredients.map((ingredient, index) => 
+                    <Ingredient key={index}
+                                ingredient={ingredient}/>) : 
+                    'There is no ingredients'}</ul>
+        </li>
     );
 }
