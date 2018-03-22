b19a69435e043bc4d08d4b9667f3c4cb0e50c52f
YulijaU <cenite-moment@mail.ru>
2018-03-22 19:29:07 +0700

    Removes unnecessary code from an api
    Removed code can be used when scaling the project

--- src/api/api.js
+++ src/api/api.js
@@ -10,19 +10,7 @@ const baseUrl = getBaseUrl();
 // - FCC: calls to local storage
 
 export function getRecipies() {
-    return get('recipies');
-}
-
-// export function deleteRecipe(id) {
-//     return del(`recipies/${id}`);
-// }
-
-export function editRecipe(recipe) {
-    return edRec(`recipies/${recipe.id}`, recipe);
-}
-
-function get(url) {
-    return fetch(baseUrl + url).then(onSuccess, onError);
+    return fetch(baseUrl + 'recipies').then(onSuccess, onError);
 }
 
 export function deleteRecipe(id) {
@@ -34,22 +22,8 @@ export function deleteRecipe(id) {
     }).then(onSuccess, onError);
 }
 
-// Can't call func delete since it's reserved word
-// function del(url) {
-//     // const request = new Request(baseUrl + url, {
-//     //     method: 'DELETE'
-//     // });
-//     // return fetch(request).then(onSuccess, onError);
-//     return fetch(baseUrl + url, {
-//         method: 'DELETE',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     }).then(onSuccess, onError);
-// }
-
-function edRec(url, editedRecipe) {
-    return fetch(baseUrl + url, {
+export function editRecipe(editedRecipe) {
+    return fetch(baseUrl + `recipies/${editedRecipe.id}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
