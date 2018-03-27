7e34c07297806fb8241ab9785e51603ca4eaa5a9
YulijaU <cenite-moment@mail.ru>
2018-03-27 14:49:28 +0700

    Fixed a bag when there is no localStorage item or it's empty

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,9 +1,20 @@
 import {Recipe} from './Recipe';
 
 export const RecipesList = ({recipes, onRecipeDelete, editRecipe}) => {
+    var hasRecipes = false;
+    if (recipes) {
+        if (recipes.length === 0) {
+            hasRecipes = false;
+        } else {
+            hasRecipes = true;
+        }
+    } else {
+        hasRecipes = false;
+    }
+
     return (
         <ul>
-            {recipes.length ? recipes.map(recipe => 
+            {hasRecipes ? recipes.map(recipe => 
                 <Recipe key={recipe.id}
                         recipe={recipe}
                         onRecipeDelete={onRecipeDelete}
