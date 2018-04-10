ad8cc3c12166aa3abe7da1ba43cacfcc918a984c
YulijaU <cenite-moment@mail.ru>
2018-04-10 14:48:22 +0700

    Configures testSetup.js

---
+++ testSetup.js
@@ -0,0 +1,30 @@
+process.env.NODE_ENV = 'test';
+
+require('babel-register');
+
+require.extensions['.css'] = function() {return null;};
+require.extensions['.png'] = function() {return null;};
+require.extensions['.jpeg'] = function() {return null;};
+
+// var jsdom = require('jsdom').jsdom;
+
+var jsdom = require('jsdom');
+var {JSDOM} = jsdom;
+
+const {document} = (new JSDOM('').window);
+
+var exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = document;
+// global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+    if (typeof global[property] === 'undefined') {
+        exposedProperties.push(property);
+        global[property] = document.defaultView[property];
+    }
+});
+
+global.navigator = {
+    userAgent: 'node.js'
+};
